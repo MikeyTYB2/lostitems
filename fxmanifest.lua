4d051a1642a22934464981511ed24b57c7068084
MikeyTYB2 <97192232+MikeyTYB2@users.noreply.github.com>
2024-06-10 17:57:09 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,20 +11,15 @@ version '2.1.5'
 lua54 'yes'
 
 client_scripts {
-    'client.lua',
-    'config.lua'
+    'client.lua'
   }
   
   server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    'server.lua',
-    'config.lua'
+    'server.lua'
   }
   
   shared_scripts {
-    '@ox_lib/init.lua',
-    'config.lua'
+    '@ox_lib/init.lua'
   }
   
-  
-  escrow_ignore 'config.lua'
