237d310621b6562bca50a205e0ce0529e039de36
MikeyTYB2 <97192232+MikeyTYB2@users.noreply.github.com>
2024-06-10 20:49:43 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,11 +1,11 @@
-ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+local ESX = exports['es_extended']:getSharedObject()
+
 
 RegisterNetEvent('lostitems', function()
-    local source = source 
+    local source = source -- Get the source inside the event function
 
     local xPlayer = ESX.GetPlayerFromId(source)
-    local itemNames = xPlayer.getInventory() 
+    local itemNames = xPlayer.getInventory() -- Use the correct ESX inventory function
 
     local currentDate = os.date("%x")
     local currentTime = os.date("%I:%M %p")
